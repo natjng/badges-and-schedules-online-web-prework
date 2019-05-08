9804a49f45cb9749e684aae19f3eee174f5ce6d3
Natalie J <natsetc@gmail.com>
2019-05-08 20:00:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator.join "-n"(attendees)
+  batch_badge_creator.join(attendees) "-n"
   puts assign_rooms(attendees)
 end
